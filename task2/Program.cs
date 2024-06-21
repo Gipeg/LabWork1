299a7e0d48b4de749ab65cc1cc11b98e6ce87d36
fisp <arenatkanovna@gmail.com>
2024-06-21 21:48:00 +0300

    Добавьте файлы проекта.

---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] array = { 7, 0, -4, 3, 1, -2, 5 };
+
+int buf = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}\t");
+}
+Console.WriteLine();
+
+for (int i = array.Length - 1; i > 0; i--)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            buf = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = buf;
+        }
+    }
+
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]}\t");
+    }
+    Console.WriteLine();
+
+}
---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = { 7, 0, -4, 3, 1, -2, 5 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}\t");
+}
+Console.WriteLine();
+
+
+for (int i = 1; i < array.Length; i++)
+{
+    int temp = array[i];
+    int j = i - 1;
+    while (j >= 0 && array[j] > temp)
+    {
+        array[j + 1] = array[j];
+        array[i] = temp;
+        j--;
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}\t");
+}
+
